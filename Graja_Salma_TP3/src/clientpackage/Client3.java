3455ca0dbe82e4784dbc68c947ea62db10c29cc3
SalmaGr <Grajasalma17@gmail.com>
2023-10-29 23:54:13 +0100

    tp

--- Graja_Salma_TP3/src/clientpackage/Client3.java
+++ Graja_Salma_TP3/src/clientpackage/Client3.java
@@ -1,18 +1,24 @@
 package clientpackage;
 
+import java.io.ObjectInputStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+
+import serverPackage.Message;
 
 public class Client3 {
 	public static void main(String[] args) {
 		try {
 			//Socket socket = new Socket("localhost", 500);
 			//InetAddress inetaddress = InetAddress.getByName("ipserveur");
-			InetAddress inetaddress = InetAddress.getByName("192.168.1.7");
+			InetAddress inetaddress = InetAddress.getByName("192.168.119.96");
 			InetSocketAddress inetsoketaddress = new InetSocketAddress(inetaddress,500);
 			Socket socket = new Socket();
 			socket.connect(inetsoketaddress);
+			ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+			Message message = (Message) ois.readObject();
+			System.out.println(message.getContent());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
